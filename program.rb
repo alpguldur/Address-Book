5a4e011c5dcccf1ce65ea2bf34cf79d6c6e2f6b7
Alp Guldur <alpguldur@gmail.com>
2015-06-21 00:21:56 +0300

    list, delete, edit contact

--- program.rb
+++ program.rb
@@ -2,8 +2,11 @@ contacts = []
 
 while true
 	puts "Select an action: list | search | create | edit <ID> | delete <ID>"
-	input = gets.chomp
-	puts "Please fill out the contact attributes."
+ 
+    input = gets.chomp  
+
+    def create
+    puts "Please fill out the contact attributes."
 	puts "First Name:"
 	first_name = gets.chomp
 	puts "Last Name:"
@@ -20,4 +23,50 @@ while true
 	}	
 	contacts << contact
 	puts "The contact has been created."
-end	
+
+    def list
+    if there is contact
+        puts first_name last_name, phone_number, email
+    else
+        puts "The list is empty"
+
+    def delete
+    puts "Are you sure to delete this contact?"	
+    if Yes = gets.chomp
+        puts "The contact has been deleted."
+    else
+    	puts "The contact has not been deleted"
+
+    def edit
+    if 	there is contact
+        puts "Please fill out the contact attributes."	
+        puts "First Name:"
+	    first_name = gets.chomp
+	    puts "Last Name:"
+	    last_name = gets.chomp
+	    puts "Phone Number:"
+	    phone_number = gets.chomp
+	    puts "Email:"
+	    email = gets.chomp
+	    contact = {
+		    first_name: first_name,
+		    last_name: last_name,
+		    phone_number: phone_number,
+		    email: email
+	    }	
+	    contacts << contact
+	    puts "The contact has been updated."
+    else
+    	puts "Contact not found"
+
+
+    
+
+
+
+
+
+
+
+
+
