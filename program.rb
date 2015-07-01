5e391be5baf71d59a2e76aeb5a4c3ff50c99333a
Alp Guldur <alpguldur@gmail.com>
2015-07-02 00:44:40 +0300

    added edit, delete, search

--- program.rb
+++ program.rb
@@ -16,22 +16,67 @@ class AddressBook
     contact = {
       first_name: first_name,
       last_name: last_name,
-	  phone_number: phone_number,
-	  email: email
-    }	
+	    phone_number: phone_number,
+	    email: email
+      }	
     @contacts << contact
     puts "The contact has been created."
   end 
 
   def list
-    @contacts.each do |contact|
-      puts contact[:first_name] + " " + 
-           contact[:last_name] + ", " +
-           contact[:phone_number] + ", " +
-           contact[:email]
+    if contacts = [contact]
+      @contacts.each do |contact|
+        puts contact[:first_name] + " " + 
+             contact[:last_name] + ", " +
+             contact[:phone_number] + ", " +
+             contact[:email]
+    elsif contacts = []
+      puts "The list is empty."               
     end  	
-  end
-end 
+  end 
+
+  def edit
+    puts "Please fill out the contact attributes."
+    puts "First Name:"
+    first_name = gets.chomp
+    puts "Last Name:"
+    last_name = gets.chomp
+    puts "Phone Number:"
+    phone_number = gets.chomp
+    puts "Email:"
+    email = gets.chomp
+    contact = {
+      first_name: first_name,
+      last_name: last_name,
+      phone_number: phone_number,
+      email: email
+      } 
+    @contacts << contact
+    puts "The contact has been updated."
+  end 
+
+  def delete
+    puts "Are you sure to delete this contact?"
+    input = gets.chomp
+    if input == "Yes"
+      contacts.delete(contact)
+      puts "The contact has been deleted."
+    else
+  end  
+    
+  def search
+    puts "First Name"
+    first_name = gets.chomp
+    contacts.include? 'first_name'
+    @contacts.each do |contact|
+      puts contact[:first_name]
+    puts "Last Name"
+    last_name = gets.chomp
+    contacts.include? 'last_name'
+    @contacts.each do |contact|
+      puts contact[:first_name] + " " + contact[:last_name] 
+  end    
+end
 
 address_book = AddressBook.new
 
@@ -44,6 +89,12 @@ while true
     address_book.create
   elsif input == "list"
     address_book.list
+  elsif input == "edit"
+    address_book.edit
+  elsif input == "delete"
+    address_book.delete  
+  elsif input == "search"
+    address_book.search  
   end
 
 end  
